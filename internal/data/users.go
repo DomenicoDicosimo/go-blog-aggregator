2eb57a300e25a49dcf5c3191a8076f90248623db
domenicodicosimo <domenicdicosimo@gmail.com>
2024-08-09 17:26:36 -0300

    updated user model and handler

--- internal/data/users.go
+++ internal/data/users.go
@@ -26,3 +26,30 @@ func DatabaseUserToUser(user database.User) User {
 		Name:      user.Name,
 	}
 }
+
+type password struct{
+	plaintext *string
+	hash []byte
+}
+
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12) 
+	if err != nil {
+		return err 
+	}
+	p.plaintext = &plaintextPassword 
+	p.hash = hash
+	return nil
+}
+
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword)) 
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil 
+		default:
+			return false, err }
+	}
+	return true, nil 
+}
